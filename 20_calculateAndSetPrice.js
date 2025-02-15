201f2f3e91302ba2442b9fa2d93da40f09970aee
oooo12-git <jhdmbwy12@gmail.com>
2025-02-15 16:49:59 +0900

    [FIX] Price 부분에서 커플할인 적용될때 일반 가격 조건 동시에 출력되는 현상

--- 20_calculateAndSetPrice.js
+++ 20_calculateAndSetPrice.js
@@ -37,19 +37,19 @@ function calculateAndSetPrice(sheet, row, PRICE_KO_COLUMN, PRICE_EN_COLUMN, coup
         ko_priceText += `※ 커플 프로필 촬영 비용: ${couplePrice.toLocaleString()}원 &#128522;30% 할인 적용\n`;
         priceText += `※ Shooting fee for Couple Profile: KRW ${couplePrice.toLocaleString()} &#128522;30% discount applied\n\n`;
     }
-    if (individual_1st >= 1 && individual_2nd >= 1 && individual_3rd >= 1 && numberOfPeople === 3 && couple_profile >= 1) {
+    else if (individual_1st >= 1 && individual_2nd >= 1 && individual_3rd >= 1 && numberOfPeople === 3 && couple_profile >= 1) {
         const couplePrice = {1: 238000, 2: 343000, 3: 448000}[couple_profile];
         totalPrice += couplePrice;
-        ko_priceText += `※ 커플 프로필 촬영 비용: ${couplePrice.toLocaleString()}원 &#128522;30% 할인 적용\n`;
+        ko_priceText += `※ 커플 프로필 촬영 비용: ${couplePrice.toLocaleString()}원 &#128522;30% 할인 적용\n\n`;
         priceText += `※ Shooting fee for Couple Profile: KRW ${couplePrice.toLocaleString()} &#128522;30% discount applied\n\n`;
     }
     else if (couple_profile) {
-      const couplePrice = {1: 340000, 2: 490000, 3: 640000}[couple_profile];
-      if (couplePrice) {
-        totalPrice += couplePrice;
-        ko_priceText += `※ 커플 프로필 촬영 비용: ${couplePrice.toLocaleString()}원\n`;
-        priceText += `※ Shooting fee for Couple Profile: KRW ${couplePrice.toLocaleString()}\n\n`;
-      }
+        const couplePrice = {1: 340000, 2: 490000, 3: 640000}[couple_profile];
+        if (couplePrice) {
+            totalPrice += couplePrice;
+            ko_priceText += `※ 커플 프로필 촬영 비용: ${couplePrice.toLocaleString()}원\n\n`;
+            priceText += `※ Shooting fee for Couple Profile: KRW ${couplePrice.toLocaleString()}\n\n`;
+        }
     }
   
     // Group Profile 가격
@@ -59,14 +59,14 @@ function calculateAndSetPrice(sheet, row, PRICE_KO_COLUMN, PRICE_EN_COLUMN, coup
     if (individual_1st >= 1 && individual_2nd >= 1 && individual_3rd >= 1 && numberOfPeople === 3 && group_profile >= 1) {
         const groupPrice = {1: 280000, 2: 413000, 3: 553000}[group_profile];
         totalPrice += groupPrice;
-        ko_priceText += `※ 그룹 프로필 촬영 비용: ${groupPrice.toLocaleString()}원 &#128522;30% 할인 적용\n`;
+        ko_priceText += `※ 그룹 프로필 촬영 비용: ${groupPrice.toLocaleString()}원 &#128522;30% 할인 적용\n\n`;
         priceText += `※ Shooting fee for Group Profile: KRW ${groupPrice.toLocaleString()} &#128522;30% discount applied\n\n`;
     }
     else if (group_profile) {
       const groupPrice = {1: 400000, 2: 590000, 3: 790000}[group_profile];
       if (groupPrice) {
         totalPrice += groupPrice;
-        ko_priceText += `※ 그룹 프로필 촬영 비용: ${groupPrice.toLocaleString()}원\n`;
+        ko_priceText += `※ 그룹 프로필 촬영 비용: ${groupPrice.toLocaleString()}원\n\n`;
         priceText += `※ Shooting fee for Group Profile: KRW ${groupPrice.toLocaleString()}\n\n`;
       }
     }
